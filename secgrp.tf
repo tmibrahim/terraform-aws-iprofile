aef9fa9d25dc8466c1c86a291d940a55e9433b42
tmibrahim <ibrahim-temur@outlook.com>
2024-03-06 06:08:02 +0300

    Security groups added.

---
+++ secgrp.tf
@@ -0,0 +1,87 @@
+resource "aws_security_group" "vprofile-bean-elb-sg" {
+  name       = "vprofile-bean-elb-sg"
+  description = "Security group for Beanstalk ELB"
+  vpc_id      = module.vpc.vpc_id
+
+  egress = {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+}
+
+resource "aws_security_group" "vprofile-bastion-sg" {
+  name        = "vprofile-bastion-sg"
+  description = "Security group for Bastionisioner ec2 instance"
+  vpc_id      = module.vpc.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.MY_IP]
+  }  
+}
+
+resource "aws_security_group" "vprofile-prod-sg" {
+  name        = "vprofile-prod-sg"
+  description = "Security group for Prod instances"
+  vpc_id      = module.vpc.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    security_groups = [aws_security_group.vprofile-bastion-sg.id]
+  }
+}
+
+resource "aws_security_group" "vprofile-backend-sg" {
+  name        = "vprofile-backend-sg"
+  description = "Security group for RDS, AwctiveMQ, ElastiCache"
+  vpc_id      = module.vpc.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [""]
+  }
+
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    security_groups = [aws_security_group.vprofile-prod-sg.id]
+  }
+}
+
+resource "aws_security_group_rule" "sec-group-all-backend-itselves" {
+  type        = "ingress"
+  from_port   = 0
+  to_port     = 65535
+  protocol    = "tcp"
+  source_security_group_id = aws_security_group.vprofile-backend-sg.id
+  security_group_id = aws_security_group.vprofile-backend-sg.id
+}
